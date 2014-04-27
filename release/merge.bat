829db128eee765204a7021fafb3f0e0f84f9cd28
j-b-n <joakim@nejdeby.se>
2014-04-27 20:00:30 +0200

    Release

--- release/merge.bat
+++ release/merge.bat
@@ -1,4 +1,4 @@
 @echo on
-rem IF EXIST MP-Domoticz_UNMERGED.dll del MP-Domoticz_UNMERGED.dll
+IF EXIST MPDomoticz.dll del MPDomoticz.dll
 rem ren MP-Domoticz.dll MP-Domoticz_UNMERGED.dll 
-ilmerge /out:MPDomoticz.dll MP-Domoticz.dll Newtonsoft.Json.dll 
+ilmerge /targetplatform:"v4,C:\Windows\Microsoft.NET\Framework64\v4.0.30319" /out:MPDomoticz.dll MP-Domoticz.dll Newtonsoft.Json.dll OxyPlot.dll OxyPlot.WindowsForms.dll /lib:"C:\Program Files\Reference Assemblies\Microsoft\Framework\v3.0" /lib:"C:\Program Files (x86)\Team MediaPortal\MediaPortal" /lib:"C:\Program Files (x86)\Team MediaPortal\MediaPortal\plugins\Windows"
